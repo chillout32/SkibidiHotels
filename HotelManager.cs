864bab731e9db3efbc0ae187d51a0db50b40dc42
Tim <tim.bjorkegren@gmail.com>
2024-11-26 09:09:54 +0100

    Made a updatebooking questions
    made a function so we can now what the customers want to update

--- HotelManager.cs
+++ HotelManager.cs
@@ -30,7 +30,7 @@ public class HotelManager
     // Method to insert customer into the database
     private void InsertClientIntoDatabase(string firstName, string lastName, string email, string phoneNumber, string dob)
     {
-        string connectionString = "Host=localhost;Username=postgres;Password=7942;Database=postgres";
+        string connectionString = "Host=localhost;Username=postgres;Password=Aqws12aqwsed;Database=postgres";
 
         try
         {
@@ -59,5 +59,41 @@ public class HotelManager
         {
             Console.WriteLine($"Error: {ex.Message}");
         }
+    }
+
+    public void UpdateBooking(int bookingID, DateTime newCheckIn, DateTime newCheckOut, int totalGuests, int roomID, 
+        bool extraBed, bool allInclusive, bool halfPension)
+    {
+        
+    }
+
+    private void UpdateBookingQuestions()
+    {
+        Console.WriteLine("Whats your booking id");
+        int bookingID = int.Parse(Console.ReadLine());
+        
+        Console.WriteLine("What time do you want to check in");
+        DateTime newCheckIn = DateTime.Parse(Console.ReadLine());
+        
+        Console.WriteLine("What time do you want to check out");
+        DateTime newCheckOut = DateTime.Parse(Console.ReadLine());
+        
+        Console.WriteLine("How many guests total will come");
+        int totalGuests = int.Parse(Console.ReadLine());
+        
+        Console.WriteLine("Do you want to change room, please enter a new room ID (admin)");
+        int roomID = int.Parse(Console.ReadLine());
+        
+        Console.WriteLine("Do you want a extra bed, (yes/no)?");
+        string extraBedInput = Console.ReadLine().ToLower();
+        bool extraBed = extraBedInput == "yes";
+        
+        Console.WriteLine("Do you want allinclusive , (yes/no)?");
+        string allinclusiveInput = Console.ReadLine().ToLower();
+        bool allInclusive = allinclusiveInput == "yes";
+        
+        Console.WriteLine("Do you want halfpension included, (yes/no)?");
+        string halfpensionInput = Console.ReadLine().ToLower();
+        bool halfPension = halfpensionInput == "yes";
     }
 }
