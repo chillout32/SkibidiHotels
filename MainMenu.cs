2534e61f9108f731cc88eb221335cddb80d549fb
PATRIK DA DON <patrik.goransson55@gmail.com>
2024-12-09 11:38:24 +0100

    Final touches
    Removed SortByDstncBeach.cs and using FilterDistance.cs instead.

--- MainMenu.cs
+++ MainMenu.cs
@@ -8,7 +8,6 @@ public class MainMenu
     private RegisterCustomer _registerCustomer = new();
     private UpdateBooking _updateBooking = new();
     private SortByReview _sortByReview = new();
-    private SortByDstncBeach _sortByDstncBeach = new ();
     private CancelBookings _cancelBookings = new();
     private SortPrices _sortPrices = new();
     private RegisterBooking _registerBooking = new();
@@ -31,25 +30,22 @@ public class MainMenu
                 case "2": 
                     _sortByReview.PrintHotelsAndStars();
                     break;
-                case "3": 
-                    _sortByDstncBeach.SortByDistance();
-                    break;
-                case "4":
+                case "3":
                     _updateBooking.UpdateBookings(); 
                     break;
-                case "5":
+                case "4":
                     _sortPrices.PrintSortByPrice();
                     break;
-                case "6":
+                case "5":
                     _cancelBookings.CancelBooking();
                     break;
-                case "7":
+                case "6":
                     _registerBooking.RegisterBookings();
                     break;
-                case "8":
+                case "7":
                     _filterDistance.FilterDistanceToBeach();
                     break;
-                case "9":
+                case "8":
                     _filterDistance.FilterDistanceToCentrum();
                     break;
                 case "x":
@@ -64,13 +60,12 @@ public class MainMenu
         Console.WriteLine("--- Welcome to SkibidiHotels ---" + $"\n");
         Console.WriteLine("1. Register customer");
         Console.WriteLine("2. List Hotels and sort by reviews/stars");
-        Console.WriteLine("3. List Hotels and sort by distance to beach");
-        Console.WriteLine("4. Change details in a booking");
-        Console.WriteLine("5. List hotels and sort by price");
-        Console.WriteLine("6. Cancel a registered booking");
-        Console.WriteLine("7. Register booking");
-        Console.WriteLine("8. Filter distance to beach");
-        Console.WriteLine("9. Filter distance to centrum");
+        Console.WriteLine("3. Change details in a booking");
+        Console.WriteLine("4. List hotels and sort by price");
+        Console.WriteLine("5. Cancel a registered booking");
+        Console.WriteLine("6. Register booking");
+        Console.WriteLine("7. Filter distance to beach");
+        Console.WriteLine("8. Filter distance to centrum");
         Console.WriteLine("x. Exit program");
     }
 }
